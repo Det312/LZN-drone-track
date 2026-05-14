d61b6aa742c08213cdca0cbf18b071b488281e22
Bartlomiej Gluch <gluch_b@interia.pl>
2026-05-14 22:33:41 +0200

    Optical sensor has its full capablity. Ready to intruduce LED strip handling.

--- include/Sensors.h
+++ include/Sensors.h
@@ -6,8 +6,11 @@
 class opticalSensor {
 public:
 
+
+
     opticalSensor(uint8_t analogPin,
-                  uint32_t readIntervalMs = 50);
+                  uint32_t readIntervalMs = 50,
+                  uint32_t sampling = 5);
 
     void begin();
     
@@ -18,6 +21,18 @@ public:
 
 
 private:
-    uint32_t sampling; //number of samples in a single measurment, to increse accuracy
     uint8_t analog_pin;
+    uint32_t readInterval; //time beeteween measurements
+    uint32_t m_sampling;     //number of samples in a single distance check, reference the filteredRead() function.
+    uint32_t lastReadTime;
+    uint32_t rawAnalog;
+    
+    float distanceCm;
+
+
+    //Returns the average of analogs read specified by object properties. Too many samples may cause problems with CPU time usege
+    uint16_t filteredRead();
+
+    uint16_t convertToDistanceCm(uint16_t adcReadout);
+    
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,49 @@
 #include <Arduino.h>
-
 // DO NOT USE delay(), OTA is NOT HAPPY WITH IT
-
+// keep constructors light, hardwere limts on startup
 
 #include "OTAHandler.h"
+#include "Sensors.h"
 
+//Pin numbers!!
+const int sensorPin1;
+const int sensorPin2;
 
 OTAHandler otaHandler;
+
+//each sensor is a separate object
+//test interval and sampling values
+opticalSensor opticalSensor1(sensorPin1);
+opticalSensor opticalSensor2(sensorPin2);
+
+float distance1;
+float distance2;
 
 void setup() {
 
     Serial.begin(115200);
 
+    delay(50); //Give the cpu time to boot up before init of OTA and wifi. Exception to the rule above
+    
     otaHandler.begin(
-        "", //ssid
-        "",//password 
-        ""//hostname
+    "", //ssid
+    "",//password 
+    ""//hostname
     );
+
+    opticalSensor1.begin();
+    opticalSensor2.begin();
 }
 
 void loop() {
     otaHandler.update();
 
+    opticalSensor1.update();
+    opticalSensor2.update();
+
+    distance1 = opticalSensor1.getDistanceCm();
+    distance2 = opticalSensor2.getDistanceCm();
+
+    //add LED logic here
 
 }
