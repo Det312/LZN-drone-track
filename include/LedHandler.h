41c537dd46d60e8cde87469f5a2960608730cd3f
Bartlomiej Gluch <gluch_b@interia.pl>
2026-05-17 09:33:54 +0200

    Add implementation of LedHandler

--- include/LedHandler.h
+++ include/LedHandler.h
@@ -7,15 +7,23 @@ class LedHandler{
 public:
     LedHandler(uint8_t dataPin, uint16_t ledCount);
 
+    //Initialize led string
     void begin();
 
-    void setBrightness(uint8_t setBrightness);
+    //Change brigtness
+    void setBrightness(uint8_t brightness);
 
+    //Change color for the whole strip
+    //Mainly for debug and testing
+    //TODO:Implement better LED control, move this func to private 
     void setAll(uint8_t red, uint8_t green, uint8_t blue);
     
+    //Update leds
     void show();
 
+    //Clear led strip
     void clear();
+    
 private:
     Adafruit_NeoPixel m_strip;
 };
